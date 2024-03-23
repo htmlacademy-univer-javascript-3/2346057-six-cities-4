45b2a35cd5454b949fcd5bf9b080a7a85910ce63
Alina Saushkina <saushkina.ar@edu.spbstu.ru>
2024-03-23 22:51:00 +0300

    module5-task1

--- src/mocks/offers.ts
+++ src/mocks/offers.ts
@@ -15,14 +15,14 @@ export const offers: Offer[] = [
       }
     },
     location: {
-      latitude: 52.35514938496378,
-      longitude: 4.673877537499948,
+      latitude: 52.3909553943508,
+      longitude: 4.85309666406198,
       zoom: 8
     },
     isFavorite: true,
     isPremium: true,
     rating: 4,
-    previewImage: 'https://w.forfun.com/fetch/85/85ae14568a42dcfeb55be68307d1a797.jpeg'
+    previewImage: 'https://proprikol.ru/wp-content/uploads/2020/10/kartinki-nochnogo-goroda-2.jpeg'
   },
   {
     id: '2',
@@ -30,7 +30,7 @@ export const offers: Offer[] = [
     type: 'apartment',
     price: 120,
     city: {
-      name: 'Moscow',
+      name: 'Amsterdam',
       location: {
         latitude: 52.35514938496378,
         longitude: 4.673877537499948,
@@ -38,14 +38,14 @@ export const offers: Offer[] = [
       }
     },
     location: {
-      latitude: 52.35514938496378,
-      longitude: 4.673877537499948,
+      latitude: 52.3609553943508,
+      longitude: 4.85309666406198,
       zoom: 8
     },
     isFavorite: true,
     isPremium: false,
     rating: 3,
-    previewImage: 'https://w.forfun.com/fetch/85/85ae14568a42dcfeb55be68307d1a797.jpeg'
+    previewImage: 'https://proprikol.ru/wp-content/uploads/2020/10/kartinki-nochnogo-goroda-2.jpeg'
   },
   {
     id: '3',
@@ -53,7 +53,7 @@ export const offers: Offer[] = [
     type: 'apartment',
     price: 120,
     city: {
-      name: 'Saint P',
+      name: 'Amsterdam',
       location: {
         latitude: 52.35514938496378,
         longitude: 4.673877537499948,
@@ -61,14 +61,14 @@ export const offers: Offer[] = [
       }
     },
     location: {
-      latitude: 52.35514938496378,
-      longitude: 4.673877537499948,
+      latitude: 52.3909553943508,
+      longitude: 4.929309666406198,
       zoom: 8
     },
     isFavorite: false,
     isPremium: false,
     rating: 4.24,
-    previewImage: 'https://w.forfun.com/fetch/85/85ae14568a42dcfeb55be68307d1a797.jpeg'
+    previewImage: 'https://proprikol.ru/wp-content/uploads/2020/10/kartinki-nochnogo-goroda-2.jpeg'
   },
   {
     id: '4',
@@ -76,7 +76,7 @@ export const offers: Offer[] = [
     type: 'apartment',
     price: 120,
     city: {
-      name: 'New York',
+      name: 'Amsterdam',
       location: {
         latitude: 52.35514938496378,
         longitude: 4.673877537499948,
@@ -84,13 +84,13 @@ export const offers: Offer[] = [
       }
     },
     location: {
-      latitude: 52.35514938496378,
-      longitude: 4.673877537499948,
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
       zoom: 8
     },
     isFavorite: true,
     isPremium: false,
     rating: 2,
-    previewImage: 'https://w.forfun.com/fetch/85/85ae14568a42dcfeb55be68307d1a797.jpeg'
+    previewImage: 'https://proprikol.ru/wp-content/uploads/2020/10/kartinki-nochnogo-goroda-2.jpeg'
   },
 ];
