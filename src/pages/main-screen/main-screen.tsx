0df69c13d0d0156096573b09af0c44e2691e5d31
Alina Saushkina <saushkina.ar@edu.spbstu.ru>
2024-03-23 23:11:57 +0300

    fixed space indentation

--- src/pages/main-screen/main-screen.tsx
+++ src/pages/main-screen/main-screen.tsx
@@ -16,8 +16,8 @@ function MainScreen({ placesCount, offers }: MainScreenProps): JSX.Element {
           <div className="header__wrapper">
             <div className="header__left">
               <Link
-                  to="/"
-                  className="header__logo-link header__logo-link--active"
+                to="/"
+                className="header__logo-link header__logo-link--active"
               >
                 <img
                   className="header__logo"
