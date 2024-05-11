bbf5afffbb2caacb3129c7688aab064898712283
Alina Saushkina <saushkina.ar@edu.spbstu.ru>
2024-05-11 16:59:24 +0300

    module7-task2

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,5 +1,6 @@
 import { createAction } from '@reduxjs/toolkit';
 import { City, Offer } from '../types/offer';
+import { AuthorizationStatus } from '../const';
 
 export const changeCity = createAction<City>('changeCity');
 export const setOffersDataLoadingStatus = createAction<boolean>(
@@ -7,6 +8,9 @@ export const setOffersDataLoadingStatus = createAction<boolean>(
 );
 export const loadOffers = createAction<Offer[]>('loadOffers');
 export const setError = createAction<string | null>('setError');
+export const setAuthorizationStatus = createAction<AuthorizationStatus>(
+  'setAuthorizationStatus'
+);
 export const setSortType = createAction<string>('setSortType');
 export const setSelectedPoint = createAction<{ title: string } | null>(
   'setSelectedPoint'
